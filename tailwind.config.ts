30a4f8a430ea33cb6f0a640d921df597ec206dc6
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2025-04-05 22:01:55 -0300

    Removido plugin tailwind-scrollbar da configuração do Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,5 +86,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
